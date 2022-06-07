9b8e1eec4919c3bb841022ea2f118c8621163db4
prakaski <kiran.prakash@sunquestinfo.com>
2022-06-07 19:42:53 +0530

    added cordova

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the app is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+  BluetoothSerial],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrintService } from '../services/print.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  bluetoothList:any=[];
+  selectedPrinter:any;
+  constructor(private print: PrintService) {}
+  ngOnInit(): void {
+    this.listPrinter();
+  }
+     //This will list all of your bluetooth devices
+     listPrinter() { 
+      this.print.searchBluetoothPrinter()
+       .then(resp=>{
+   
+        //List of bluetooth device list
+        this.bluetoothList=resp;
+        console.log(this.bluetoothList);
+    });
+  }
+
+  selectPrinter(macAddress)
+  {
+    //Selected printer macAddress stored here
+    this.selectedPrinter=macAddress;
+  }
 
 }
